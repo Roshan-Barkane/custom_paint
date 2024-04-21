d38d7c0ff92123aef6cac05ec84ebefab87539c3
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-21 16:01:48 +0530

    use circle property done!

--- lib/activity/drowcircle.dart
+++ lib/activity/drowcircle.dart
@@ -2,16 +2,14 @@
 import 'package:flutter/material.dart';
 
 class DrowCircle extends CustomPainter {
-  /* CustomPainter is override two mathods paint and shouldRepaint
-   1. paint mathods- its take to parameter convas and size.
-   2. shouldRepaint-  */
+  /* PaintingStyle have to value stroke and fill  */
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
     Paint paint = Paint();
     paint.strokeWidth = 3;
     paint.color = Colors.black;
-    //canvas.drawLine(Offset.zero, Offset(size.width, 0), paint);
+    //paint.style = PaintingStyle.stroke;
+    paint.style = PaintingStyle.fill;
     canvas.drawCircle(Offset(size.width / 2, size.height / 2), 100.0, paint);
   }
 
